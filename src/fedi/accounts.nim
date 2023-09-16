a970a658e4e2fbbe261273de90eb841b7476cca7
nsaspy <nsaspy@fedora.email>
2023-09-16 16:12:14 -0400

    add macro to capture default arguments

--- src/fedi/accounts.nim
+++ src/fedi/accounts.nim
@@ -3,6 +3,10 @@ import httpclient
 import asyncdispatch
 import json
 import strformat
+import private
+import uri
+
+
 proc verifyAccountCreds*(client: FediClient or AsyncFediClient): Future[JsonNode] {.multisync.} =
   let url = client.makeUrl("/api/v1/accounts/verify_credentials")
   let req = await client.hc.get(url)
@@ -22,18 +26,33 @@ proc getAccountInfo*(client: FediClient or AsyncFediClient, accountId: string): 
 
   return (await req.body).parseJson
 
-proc getTimeline*(client: FediClient or AsyncFediClient, instance: string, local = false, remote = false, limit = 40): Future[JsonNode] {.multisync.} =
-  let req = await client.hc.get(fmt"{instance}/api/v1/timelines/public")
+
+proc getTimeline*(client: FediClient or AsyncFediClient, instance: string, local, remote, onlyMedia: bool = false, limit = 40, maxId, minId, sincId: int64 = 0): Future[JsonNode] {.captureDefaults, multisync.} =
+  let req = await client.hc.get(fmt"{instance}/api/v1/timelines/public?" & encodeQuery createNadd(
+    newseq[DoubleStrTuple](),[
+    local,
+    remote,
+      limit],
+    defaults
+  ))
   await castRateLimit(res=req, client=client.hc)
   castError req
   return (await req.body).parseJson
 
-proc getTimeline*(client: FediClient or AsyncFediClient, local = false, remote = false, limit = 40): Future[JsonNode] {.multisync.} =
-  let url = client.makeUrl("/api/v1/timelines/public")
-  let req = await client.hc.get(url)
+
+proc getTimeline*(client: FediClient or AsyncFediClient, local, remote, onlyMedia: bool = false, limit = 20, maxId, minId, sincId: int64 = 0): Future[JsonNode] {.captureDefaults, multisync.} =
+  let req = await client.hc.get(client.makeUrl("/api/v1/timelines/public?" & encodeQuery createNadd(
+    newseq[DoubleStrTuple](),[
+    local,
+    remote,
+      limit],
+    defaults
+  )))
   await castRateLimit(res=req, client=client.hc)
   castError req
   return (await req.body).parseJson
+
+
 
 proc getStatuses*(client: FediClient or AsyncFediClient, accountId: string, limit: int = 20, onlyMedia: bool = false, excludeReplies: bool = false ): Future[JsonNode] {.multisync.} =
   let url = client.makeUrl(fmt"/api/v1/accounts/{accountId}/statuses")
@@ -63,3 +82,8 @@ proc lookupAccount*(client: FediClient or AsyncFediClient, acct: string): Future
   castError req
   let data = (await req.body).parseJson
   return data
+
+
+when isMainModule:
+  test(1, 2)
+  #echo encodeQuery({"a": "", "b": "2"}, true)
---
+++ src/fedi/private.nim
@@ -0,0 +1,103 @@
+import macros, json
+import macroutils, macroplus, strutils
+
+type DoubleStrTuple* = tuple[key: string, val: string]
+
+macro captureDefaults*(routine): untyped =
+  when defined(docs):
+    routine[^1] = routine[^1].extractDocCommentsAndRunnables
+    return routine
+
+  ## this macro captures all default valued arguments and save them in a let defaults: tulple[arg1: defaultVal, arg2: ...,]
+  ## in the head of the routine
+
+  routine.expectKind RoutineNodes
+
+  # let defaults {.global.} = (a:2, b:3)
+  #
+  # LetSection:
+  #   IdentDef:
+  #    PragmaExpr
+  #      Ident "defaults"
+  #      Pragma
+  #        Ident "global"
+  #     Empty
+  #     TupleConstr:
+  #       ExprColonExpr:
+  #         Ident "a"
+  #         IntLit 2
+  #       ExprColonExpr:
+  #         Ident "b"
+  #         IntLit 3
+
+  var defs = quote:
+    let defaults {.used, inject.} =  nil # use global pragma to initiate it at soon as program started
+
+  defs[0][IdentDefDefaultVal] = newNimNode(nnkTupleConstr)
+
+  for arg in routine[RoutineFormalParams][FormalParamsArguments]:
+    if arg[IdentDefDefaultVal].kind != nnkEmpty:
+      for ident in arg[IdentDefNames]:
+        defs[0][IdentDefDefaultVal].add newNimNode(nnkExprColonExpr).add(
+          ident,
+          arg[IdentDefDefaultVal]
+        )
+
+  routine[RoutineBody].insert 0, defs
+  return routine
+
+func getStrName(n: NimNode): string=
+  case n.kind:
+  of nnkident: n.strVal
+  of nnkAccQuoted: n[0].strVal
+  else:
+    raise newException(ValueError, "not allowed")
+
+macro addIfIsNotDefault*(acc: var JsonNode, checks, defaults): untyped =
+  ## checks bracket [ tuple( currentValue[0], defaultValue[1] ) ]
+  ## if whatYouWannaReturnIfItwasValid was not there we assume that he wants to return currentValue
+  checks.expectKind nnkBracket
+  defaults.expectKind {nnkIdent, nnkSym}
+  result = newstmtlist()
+
+  for item in checks.children:
+    item.expectKind {nnkIdent, nnkAccQuoted}
+
+    result.add do: superQuote:
+      if `item` != `defaults`.`item`:
+        `acc`[`item.getStrName`] = % `item`
+
+macro addIfIsNotDefault*(acc: var seq[DoubleStrTuple], checks, defaults): untyped =
+  ## checks bracket [ tuple( currentValue[0], defaultValue[1], whatYouWannaReturnIfItwasValid[2] ) ]
+  ## if whatYouWannaReturnIfItwasValid was not there we assume that he wants to return currentValue
+  checks.expectKind nnkBracket
+  result = newstmtlist()
+
+  for item in checks.children:
+    item.expectKind {nnkIdent, nnkAccQuoted}
+
+    result.add do: superQuote:
+      if `item` != `defaults`.`item`:
+        `acc`.add (`item.getStrName`, $ `item`)
+
+template createNadd*(data, checks, default): untyped=
+  block:
+    var res = data
+    res.addIfIsNotDefault(checks, default)
+    res
+
+
+# TODO move it to the tests
+when isMainModule:
+  proc hey(a: bool, `b` = "hello", c = 2) {.captureDefaults.} =
+    var list: seq[DoubleStrTuple]
+    list.addIfIsNotDefault [`b`, c], defaults
+
+    echo list
+
+  hey(false, "do not show c")
+
+  proc test(a: bool, b: string, c = 2) {.captureDefaults.} =
+    echo "z: ", defaults.c
+
+  test(false, "das")
